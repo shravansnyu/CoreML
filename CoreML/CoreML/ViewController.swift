67d412986cc4b895f5f8f17ef833bcc2fbe4e4e2
Shravan K <shravank@Shravans-MBP.Dlink>
2017-12-22 18:08:49 +0530

    First Upload onto Github

---
+++ CoreML/CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML
+//
+//  Created by Shravan K on 12/22/17.
+//  Copyright © 2017 GoDimensions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
