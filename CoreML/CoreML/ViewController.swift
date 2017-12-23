f808a1961fe42e9496b5fa33d04954e436b51b83
Sanjay Kumaran <sanjaykumaran@Sanjays-MacBook-Pro.local>
2017-12-23 10:59:12 +0530

    change of app name

--- CoreML/CoreML/ViewController.swift
+++ CoreML/CoreML/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var genderPrediction: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.delegate = self
     }
-
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func features(_ string:String) -> [String:Double] {
+        guard !string.isEmpty else {
+            return [:]
+        }
+        let string = string.lowercased()
+        var keys = [String]()
+        
+        keys.append("firstLetter1=\(string.prefix(1))")
+        keys.append("firstLetter2=\(string.prefix(2))")
+        keys.append("firstLetter3=\(string.prefix(3))")
+        keys.append("lastLetter1=\(string.suffix(1))")
+        keys.append("lastLetter2=\(string.suffix(2))")
+        keys.append("lastLetter3=\(string.suffix(3))")
+        
+        return keys.reduce([String: Double]()) { (result, key) -> [String: Double] in
+            var result = result
+            result[key] = 1.0
+            return result
+        }
+       
+    }
+    
+    func predictGender(_ name:String) -> String? {
+        return "hi"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         //If any memory warning was recieved.
